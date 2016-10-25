f97ab14f53ea7d20f7a1c02e6b85c443ad39a944
Rezzolve Express <rezzolveexpress@gmail.com>
2016-10-25 17:35:55 -0300

    finalizando por hoje

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -334,7 +334,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
-    interval: 1200,
+    interval: 800,
     pause: 'hover',
     wrap: true,
     keyboard: true
